fc0514a713abd53fe8aa8f473eb9ff12d9fc6782
Manuel Sanchez del Rio <srio@esrf.eu>
2023-08-11 09:22:40 +0200

    debugging Johansson crystals

--- setup.py
+++ setup.py
@@ -175,7 +175,7 @@ else:
 
 setup(
     name='shadow3',
-    version='23.4.28',
+    version='23.8.11',
     packages=['Shadow'],
     url='http://github.com/oasys-kit/shadow3',
     license='http://www.gnu.org/licenses/gpl-3.0.html',
